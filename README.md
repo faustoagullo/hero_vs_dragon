246bae24571e48db2415699622f39f369e7e3538
faustoagullo <fagullo.ieu2020@student.ie.edu>
2022-03-10 12:05:41 +0100

    Hero vs Dragon Homework Assignment

--- README.md
+++ README.md
@@ -6,6 +6,14 @@
 
 Please describe the rules in this list:
 1. The hero battles the dragon
-2. The hero and the dragon have a set of "hit points"
+2. The hero and the dragon are assigned a set of "hit points"
 3. The hit points value is read from the user
-4. _keep describing the problem_
+4. The hero and the dragon are also assigned a set of "max_damage"
+5. The "max_damage" value is also read from the user
+6. The battle begins!
+7. In each round the dragon and the hero have one opportunity to attack (dragon goes first)
+8. The value of the attack will be a randomly generated number ranging from 1 to the max_damage read by player
+9. The dragon and the hero take turns dealing damage to one another until one of them is victorious
+10. If dragon dies announce the hero as the winner
+11. If hero dies announce the dragon as the winner
+
--- game.py
+++ game.py
@@ -4,33 +4,57 @@ import random
 # Please read into the variables below the correct numbers. Use try and except to catch error.
 # a simple example would be:
 # hero_hp = int(input("how many hp does the hero have?"))
-hero_hp = 50
-dragon_hp = 100
-hero_max_dmg = 20
-dragon_max_dmg = 10
 
-print("The dragon with", dragon_hp, "hp attacks out hero with", hero_hp, "hp")
+try:
+    hero_hp = int(input("How many hp does the hero have?"))
+    print("Hero has", hero_hp,"hp")
+except ValueError:
+    print("Please insert a real number")
+
+try:
+    dragon_hp = int(input("How many hp does the dragon have?"))
+    print("Dragon has", dragon_hp,"hp")
+except ValueError:
+    print("Please insert a real number")
+
+try:
+    hero_max_dmg = int(input("What is the maximum damage our hero can deal?"))
+    print("The maximum damage of our hero is", hero_max_dmg)
+except ValueError:
+    print("Please insert a real number")
+
+try:
+    dragon_max_dmg = int(input("What is the maximum damage the dragon can deal?" ))
+    print("The maximum damage of the dragon is", dragon_max_dmg)
+except ValueError:
+    print("Please insert a real number")
+
+print("The dragon with", dragon_hp, "hp attacks our hero with", hero_hp, "hp")
 
 # add a While for an infinite block
 # here goes the while:
+while True:
     dragon_attack = random.randint(1, dragon_max_dmg)
     # here you need to update the hero hp, you need to subtract the damage that the dragon did
     # add code on this line
-
+    hero_hp = hero_hp - dragon_attack
     print("The dragon does", dragon_attack, "damage and the hero has", hero_hp, "hp left")
     # add an if condition to check if the hero was killed by the dragon
     # here goes the if
+    if hero_hp <= 0:
         print("Unfortunately the dragon killed our hero. RIP sir Bravealot")
         break
 
     hero_attack = random.randint(1, hero_max_dmg)
     # here you need to update the dragon hp, you need to subtract the damage that the hero did
     # add code on this line
-
+    dragon_hp = dragon_hp - hero_attack
     print("The hero does", hero_attack, "damage and the dragon has", dragon_hp, "hp left")
     # add an if condition to check if the dragon was killed by the hero
     # here goes the if
+    if dragon_hp <= 0:
         print("Our valiant hero has slain the dragon!")
+        print("Congratulations!")
         break
 
     input("Round over. Press any key")
